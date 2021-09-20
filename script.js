0cc52c4053578080940b71fd7d02576dfc862a7a
Sirichot ketkham <FirstSirichot_2805@hotmail.com>
2021-09-21 00:52:08 +0700

    Test01

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>2){
+          $('.navbar').addClass('sticky');
+        }else{
+          $('.navbar').removeClass('sticky');
+        }
+    });
+    //toggle menu
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+//Typing amimation script
+
+var typed = new Typed(".typing",{
+  strings: ["Creator","Designer","Devloper","Teacher"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop: true
+});
