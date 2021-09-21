cc6d0e28fca84c69e0970a15bbb8f0bfe8f859d4
Sirichot2805 <85666354+Sirichot2805@users.noreply.github.com>
2021-09-22 01:11:33 +0700

    add flex about

--- script.js
+++ script.js
@@ -11,11 +11,19 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
 });
 
 //Typing amimation script
 
 var typed = new Typed(".typing",{
+  strings: ["Creator","Designer","Devloper","Teacher"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop: true
+});
+
+var typed01 = new Typed(".typing-02",{
   strings: ["Creator","Designer","Devloper","Teacher"],
   typeSpeed:100,
   backSpeed:60,
